ea97b07fdcda2c1fb4627f0babe40e57de31e13d
Muhammad Moiz Younis <muhammadmoizyounis6547@gmail.com>
2023-11-24 23:40:34 +0500

    Professional added in post

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -30,6 +30,11 @@ class PostResource extends JsonResource
                 "name" => $this->client->name,
                 "image" => asset('storage/' . $this->client->image),
             ],
+            "professional" => $this->professional_id ? [
+                "id" => $this->professional->id,
+                "name" => $this->professional->name,
+                "image" => asset('storage/' . $this->professional->image),
+            ] : null,
             "is_requested" => auth()->user()->role == Constant::PROFESSIONAL ? (!empty(PostProposal::where('post_id', $this->id)->where('professional_id', auth()->user()->id)->first()) ? true : false) : null,
             "is_client_reviewed" => auth()->user()->role == Constant::CLIENT ? (!empty(ProfessionalRating::where('post_id', $this->id)->where('client_id', auth()->user()->id)->first()) ? true : false) : null,
             "is_professional_reviewed" => auth()->user()->role == Constant::PROFESSIONAL ? (!empty(ClientRating::where('post_id', $this->id)->where('professional_id', auth()->user()->id)->first()) ? true : false) : null
